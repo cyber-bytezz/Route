dd690f979a6cadce7524584d6428adcba331ca4b
Aro Barath Chandru <130319315+cyber-bytezz@users.noreply.github.com>
2024-10-28 22:48:04 +0530

    Create cn.js

---
+++ cn.js
@@ -0,0 +1,166 @@
+export const Collages = [
+  {
+    _id: "doc1",
+    name: "Jeppiaar Engineering College",
+    image:
+      "https://www.static-contents.youth4work.com/university/Documents/Colleges/newsEvent/7ff4495c-1948-4f05-ace0-96837380a810.jpg",
+    speciality: "Engineering",
+    place: "Tamil Nadu",
+    est: "Estd 2001",
+    anothername: "KG-PTU, Jalandhar",
+    agre: "AICTE, NBA, NAAC-A",
+    location:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.643931980014!2d80.2179173750746!3d12.866259387439543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525bbace477413%3A0xf70e4decbbd645ea!2sJeppiaar%20Engineering%20College!5e0!3m2!1sen!2sin!4v1729173979491!5m2!1sen!2sin",
+    experience: "1 Year",
+    rating: "4", // Add this for the star rating
+    reviews: "2512",
+    degree: "MBBS",
+    field1: "ECE",
+    field2: "CSE",
+    field3: "IT",
+    field4: "Bio-Technology",
+    field5: "Mechanical Engineering",
+    field6: "Aeronautical Engineering",
+    field7: "Law",
+    field8: "Art & Science",
+    fees: 50,
+    about:
+      "Jeppiaar Engineering College is renowned for its commitment to producing skilled engineers who are ready to tackle global challenges...",
+    questions: [
+      {
+        question: "What is the admission process?",
+        answer: "The admission process involves applying online...",
+      },
+      {
+        question: "What are the college's facilities?",
+        answer: "The college offers state-of-the-art laboratories, library...",
+      },
+      {
+        question: "What is the fee structure?",
+        answer:
+          "The fee structure varies by course, starting from â‚¹120,000 per year.",
+      },
+    ],
+    courses: [
+      {
+        name: "BBA",
+        duration: "3 Year",
+        type: "Full Time",
+        category: "UG",
+        fees: "â‚¹157,000/- Per Year",
+      },
+      {
+        name: "BCA",
+        duration: "3 Year",
+        type: "Full Time",
+        category: "UG",
+        fees: "â‚¹53,666/- Per Year",
+      },
+    ],
+    placement: {
+      2023: {
+        description: "Another University excelled in 2023 placements...",
+        topRecruiters: [
+          "Google",
+          "Facebook",
+          "TCS",
+          "Wipro",
+          "Infosys",
+          "Capgemini",
+        ],
+        careerCenter:
+          "The Career Resource Center at Another University ensured top placement results in 2023.",
+        totalOffers: 1025,
+      },
+      2024: {
+        description:
+          "Another University broke its previous record with more offers in 2024...",
+        topRecruiters: ["Amazon", "Apple", "IBM", "Google", "TCS", "HCL"],
+        careerCenter:
+          "The 2024 placements saw another round of success for Another University with over 1000 offers.",
+        totalOffers: 1100,
+      },
+    },
+    address: {
+      line1: "17th Cross, Richmond",
+      line2: "Circle, Ring Road, London",
+    },
+    facilities: [
+      {
+        name: "Campus",
+        icon: "https://cdn-icons-png.flaticon.com/128/182/182336.png",
+        rating: 4.5
+      },
+      {
+        name: "Comp Labs",
+        icon:"https://cdn-icons-png.flaticon.com/512/2393/2393605.png",
+        rating: 4.7
+      },
+      {
+        name: "Sports",
+        icon:"https://cdn-icons-png.flaticon.com/512/7438/7438654.png",
+        rating: 4.6
+      },
+      {
+        name: "Gym",
+        icon:"https://cdn-icons-png.flaticon.com/128/7984/7984880.png",
+        rating: 4.8
+      },
+      {
+        name: "Medical",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_R_yXNKqVkZkUW6c4lOkwIxSi2nurpSeDGQ&s",
+        rating: 4.2
+      },
+      {
+        name: "Cafeteria",
+        icon:"https://thumbs.dreamstime.com/b/cafeteria-diner-vector-signs-coffee-cup-smoke-fork-knife-plate-icon-symbols-restaurant-cafe-icons-cafeteria-123781463.jpg",
+        rating: 4.4
+      },
+      {
+        name: "Laboratory",
+        icon:"https://cdn-icons-png.flaticon.com/512/8711/8711362.png",
+        rating: 4.9
+      },
+      {
+        name: "Library",
+        icon:"https://cdn-icons-png.flaticon.com/512/9043/9043296.png",
+        rating: 4.7
+      },
+      {
+        name: "Auditorium",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7NFqGsDLmfJIEeyfHeq07mAFa_VpitXxDbQ&s",
+        rating: 4.3
+      },
+      {
+        name: "Hostel",
+        icon:"https://thumbs.dreamstime.com/b/hotel-icon-vector-set-white-background-eps-331976399.jpg",
+        rating: 4.6
+      }
+    ],       
+    rankings: [
+      {
+        year: "2023",
+        award:
+          "Jeppiaar Engineering College received the Best Engineering College Award.",
+      },
+      {
+        year: "2024",
+        award: "Awarded for highest placement offers in Tamil Nadu.",
+      },
+      {
+        year: "2024",
+        award: "Awarded for highest placement offers in Tamil Nadu.",
+      },
+    ],
+
+    highlights: {
+      Established: "2001",
+      Type: "Private",
+      Affiliation: "AICTE, NBA, NAAC-A",
+      Location: "Tamil Nadu",
+      "Exam Frequency": "Annual",
+      "Courses Offered": "Engineering, Technology, Science",
+      "Contact Details": "contact@jeppiaar.edu.in",
+    },
+  },
+  
